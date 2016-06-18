70c5918313853bfaf9645fbdf974b091cbe89194
sailajaappari <sailajakadali@gmail.com>
2016-06-18 17:01:34 +0530

    next commit

--- src/cljs/todomvc/core.cljs
+++ src/cljs/todomvc/core.cljs
@@ -1,6 +1,7 @@
 (ns todomvc.core
     (:require [reagent.core :as r]
-              [ajax.core :as ajax]))
+              [ajax.core :as ajax]
+              [secretary.core :as secretary :refer-macros [defroute]]))
 
 (enable-console-print!)
 
@@ -9,22 +10,36 @@
 
 (def retrieve-item (r/atom nil))
 
-(def atodos (r/atom nil))
+(def item (r/atom nil))
 
-(def ctodos (r/atom nil))
+(def cnt (r/atom 0))
+
+(defn add-todo [text]
+  (let [id (swap! cnt inc)]
+    (sorted-map :id id :task text :active true)))
 
 ;;Add to vector
 (defn add-to-vector [item]
   (swap! todos conj item))
 
 (def adding (do
-              (add-to-vector {:id 1 :task "clojure" :active true})
-              (add-to-vector {:id 2 :task "html" :active false})
-              (add-to-vector {:id 3 :task "javascript" :active false})))
+              (add-to-vector (add-todo "clojure"))
+              (add-to-vector (add-todo "html"))
+              (add-to-vector {:id cnt :task "javascript" :active false})))
+
+(defn display [todos1]
+  [:div
+   [:table
+    (for [i (range 0 (count @todos1))]
+      ^{:key (get-in @todos1 [i :id])}
+      [:tr
+       [:td (get-in @todos1 [i :task])]])]])
 
 ;; Remove from vector
 (defn remove-from-vector [index]
-  (reset! todos (vec (concat (subvec @todos 0 index) (subvec @todos (+ index 1))))))
+  (do
+    (reset! todos (vec (concat (subvec @todos 0 index) (subvec @todos (+ index 1)))))
+    (display todos)))
 
 ;;Retrieve from vector
 (defn retrieve-from-vector [id1]
@@ -35,30 +50,21 @@
          (reset! retrieve-item (subvec @todos i (+ i 1)))
          (recur (inc i))))))
 
-(defn display [todos1]
-  [:div
-   [:table
-    (for [i (range 0 (count @todos1))]
-      ^{:key (get-in @todos1 [i :id])}
-      [:tr
-       [:td (get-in @todos1 [i :task])]])]])
+
 
 ;;Display all todos
-(defn all-todos [todos]
+(defn all-todos [todos1]
   (display todos1))
 
 ;;Display acitve todos
-(defn active-todos [todos]
-  (do
-    (reset! atodos (vec (filter (fn [x] (= (:active x) true)) @todos1)))
-    (display atodos)))
+(defn active-todos [todos1]
+   (vec (filter (fn [x] (= (:active x) true)) @todos1)))
+     
 
 
 ;;Display complete todos
 (defn complete-todos [todos1]
-  (do
-    (reset! ctodos (vec (filter (fn [x] (= (:active x) false)) @todos1)))
-    (display ctodos)))
+  (vec (filter (fn [x] (= (:active x) false)) @todos1)))
 
 
 
@@ -69,10 +75,26 @@
 
 
 (defn home []
-  [:div
-   [:div
-    [:h1 "Todo List "]]
-    [:p (complete-todos todos)]]) 
+  (let [rtodos (r/atom [])
+        task (r/atom nil)]
+    (fn []
+       [:div
+        [:div
+         [:h1 "Todo List "]]
+        [:div
+         [:span
+          [:input {:type "text"
+                     :on-change #(reset! task (-> % .-target .-value))}]
+          [:button {:on-click #(swap! todos conj (add-todo @task))} "Add"]]]
+
+            ;; [:p (all-todos todos)]
+        [:div
+         [:p (display rtodos)]]
+        [:span 
+         [:button {:on-click #(reset! rtodos @todos)} "All"]
+         [:button {:on-click #(reset! rtodos (active-todos todos))} "Active"]
+         [:button {:on-click #(reset! rtodos (complete-todos todos))} "Complete"]]
+        [:p (str @rtodos)]]))) 
 
 
 (defn ^:export main []
